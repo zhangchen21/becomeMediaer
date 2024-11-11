3819317c4a00ca9e16ea033e1216ab98258e5cb4
Chen Zhang <244551528@qq.com>
2024-11-11 11:22:25 +0800

    feat: 新闻前沿增加AI

--- backend/src/TouTiaoNew/common/index.js
+++ backend/src/TouTiaoNew/common/index.js
@@ -20,8 +20,8 @@ export async function getNews () {
     const document = await getHtmlBody(newsLink);
     const article = document.querySelector('div.article');
 
-    // 找到第一个 .img_wrapper 元素
-    const firstImgWrapper = article.querySelector('.img_wrapper');
+    // 找到第 2 个 .img_wrapper 元素
+    const SecondImgWrapper = article.querySelectorAll('.img_wrapper')[1];
 
     const shouldSkip = [
       article.querySelector('style'),
@@ -32,9 +32,9 @@ export async function getNews () {
     const shouldBreak = [
       article.querySelector('script'),
       article.querySelector('#app-kaihu-stock'),
-      article.querySelector('#clearfix appendQr_wrap'),
+      article.querySelector('.appendQr_wrap'),
+      article.querySelector('.app-kaihu-qr'),
     ]
-    
 
     // 创建一个 div 来存储 .img_wrapper 之前的内容
     const div = document.createElement('div');
@@ -42,27 +42,58 @@ export async function getNews () {
     // 遍历 .article 的子节点，直到找到 .img_wrapper
     let currentNode = article.firstChild;
     while (currentNode) {
-        // 保存下一个节点，因为 appendChild 会改变 currentNode 的兄弟节点顺序
+      // 保存下一个节点，因为 appendChild 会改变 currentNode 的兄弟节点顺序
       const nextNode = currentNode.nextSibling;
-      // 将当前节点移动到 div 中
-      div.appendChild(currentNode);
 
-      if (currentNode === firstImgWrapper) {
-        break;
-      }
-
-      // 移动到下一个兄弟节点
-      currentNode = nextNode;
       if (shouldSkip.includes(currentNode)) {
+        currentNode = nextNode;
         continue;
       }
       if (shouldBreak.includes(currentNode)) {
         break;
       }
+
+      // 将当前节点移动到 div 中
+      div.appendChild(currentNode);
+
+      // 只保留到第一个 img 的内容
+      if (currentNode === SecondImgWrapper) {
+        break;
+      }
+
+      // 移动到下一个兄弟节点
+      currentNode = nextNode;
     }
       
-      // 此时 div 包含了 .img_wrapper 之前的所有内容
-      newContent = div.outerHTML.trim();
+    // AI 处理 div 内容，对随机 n 个 p 标签内容进行修改
+    const ps = [...div.querySelectorAll('p')];
+    let shouldAI = [];
+    // n 取随机数，不超过数量的 80%
+    let randomCount_p = Math.min(Math.floor(Math.random() * ps.length) + 1, Math.floor(ps.length * 0.8));
+    console.log("【改写句子数量】", randomCount_p)
+    while(randomCount_p >= 0) {
+      shouldAI.push(randomItem(ps));
+      randomCount_p --;
+    }
+    
+    const AIPromises = [];
+    shouldAI.forEach((p, index) => {
+      const innerText = p.textContent;
+
+      const promise = new Promise(async (resolve) => {
+        // console.log("改写前：", innerText)
+        const newText = await getAI(`请用改变描述方式、用词、句式、适当的删除和增加内容等方式，大幅度改写以下内容："${innerText}"。（要求：返回内容的格式为：纯净的内容字符串）`);
+        // console.log("改写后：", newText)
+        p.innerHTML = newText;
+        resolve();
+      })
+
+      AIPromises.push(promise);
+    })
+    await Promise.all(AIPromises);
+
+    // 此时 div 包含了 .img_wrapper 之前的所有内容
+    newContent = div.outerHTML.trim();
 
     const res = `<b>【财经前沿】</b><br /><b>${newTitle}</b><br />${newContent}<br /><b>【赚钱思维】</b>`;
     return { title: newTitle, text: res};
